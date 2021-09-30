09754d0b261adbfa305a8f47384d19737e0b569e
tataTok <tanushka777tokareva900@gmail.com>
2021-09-30 21:51:57 +0500

    File modification.

--- askname.c
+++ askname.c
@@ -10,3 +10,12 @@ void askname(char *first, char *last)
     printf("Now enter your last name: ");
     gets(last); /* buffer overflow? what's that? */
 }
+int main(int argc, char **argv)
+{
+    char first[255], last[255];
+
+    askname(first, last);
+
+    printf("Hello, %s %s!\n", first, last);
+    return 0;
+}
